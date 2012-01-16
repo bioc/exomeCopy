23972564aeca756ba2f6fedc146d10fa058b633b
Michael Love <michaelisaiahlove@gmail.com>
2012-01-16 10:27:36 +0000

    version bump
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/exomeCopy@62071 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/exomeCopy.c
+++ src/exomeCopy.c
@@ -53,8 +53,8 @@ void viterbi( int *ptmax, int *pnstates, double *start_probs, double *a, double 
   int i, j, t;
   int tmax = ptmax[0];
   int nstates = pnstates[0];
-  double vsum = 0;
-  double running_max = 1.0e10;
+  double vsum = 0.0;
+  double running_max = 0.0;
 
   R_CheckUserInterrupt();
 
